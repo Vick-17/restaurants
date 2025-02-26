839eb692fd4fec6e7cf9da9332628f73781c6d69
Vick-17 <vega.victor.0712@gmail.com>
2025-02-26 09:07:50 +0100

    FIX: UserMapper

--- src/main/java/ad/ya/restaurants/users/UserMapper.java
+++ src/main/java/ad/ya/restaurants/users/UserMapper.java
@@ -4,14 +4,6 @@ import org.mapstruct.Mapper;
 
 @Mapper
 public interface UserMapper {
-<<<<<<< HEAD
- UserDto toDto(User user);
- User toEntity(UserDto dto);
-    
-} 
-=======
-
     UserDto toDto(User user);
     User toEntity(UserDto userDto);
 }
->>>>>>> main
